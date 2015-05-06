76a5578839c61ab35922c53556afc0b8092c664e
Ville Niittunen <ww.niittunen@hotmail.com>
2015-05-06 16:44:41 +0300

    Add a zombie sprite.
    Made a zombie sprite with front and back in Aseprite.

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 var kekkonen = 56;
 var lista = [3, true, undefined, 'j'];
-lista.append("kello");
-log(lista);
+lista.push("kello");
+console.log(lista);
+
+while(lista[lista.length -1].length > 0){
+  if(lista[lista.length -1] === true){
+    console.log("On totta");
+  }
+  else{
+    console.log("Ei ole totta");
+  }
+  console.log(lista[lista.length -1].shift());
+}
